6569e417a8a9e32d947d1c1a9d132692280d4bd6
seungjoon1011 <sg15400@gmail.com>
2025-07-09 14:14:10 +0900

    model & repository

---
+++ src/main/java/com/example/simple_board/repository/PostRepository.java
@@ -0,0 +1,6 @@
+package com.example.simple_board.repository;
+
+import com.example.simple_board.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Integer> {}
